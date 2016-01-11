c23ca1d7cc24b84a15a0e2ff1c224801f43623af
Caleb Sattgast <calebsattgast@gmail.com>
2016-01-11 13:28:03 -0800

    built out and tested first couple of objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 // My javascript lives in here
+// OBJECT LITERALS
+console.log('my js is connected');
+
+// Test object, just for funsies
+var rubberDuck = {
+  material: 'rubber',
+  color: 'yellow',
+  squeaks: false,
+  favoriteFoods: ['duckweed', 'peanut butter', 'little children'],
+  says: function() {
+    console.log('quack');
+  }
+};
+
+// Objects for coffee store locations
+// Build just one for starters
+var pike = {
+  parameter1: 'value',
+  parameter2: 'value',
+  parameter3: true
+};
+
+var paragraphEl = document.createElement('p');
+var tableEl = document.createElement('table');
+var tableDataEl = document.createElement('td');
+
+paragraphEl.innerHTML = rubberDuck.says();
+document.body.appendChild(paragraphEl);
