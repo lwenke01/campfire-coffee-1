10233c0ad9426688662593a82622bb6538d05031
Caleb Sattgast <calebsattgast@gmail.com>
2016-01-13 09:35:54 -0800

    cleaned up a little

--- js/app.js
+++ js/app.js
@@ -16,8 +16,7 @@ var rubberDuck = {
 // Array for hours of the day the coffee shops are open
 var openTimes = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12 noon', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 
-// Parent object for coffee shop locations
-
+// Parent constructor object for coffee shop locations
 function CoffeeShop(shopName, minCust, maxCust, cupsCustAvg, lbsCustAvg) {
   this.shopName = shopName;
   this.minCust = minCust;
